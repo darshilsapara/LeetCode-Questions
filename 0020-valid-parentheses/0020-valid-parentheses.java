4f77afae7d89d00cb920665448a381abeca10df5
darshilsapara <d.14sapara@gmail.com>
2026-09-20 00:02:08 +0530

    Update 0020-valid-parentheses.java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>(); 
+        Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()) {   
             if(c=='('||c=='{'||c=='['){
                 stack.push(c);
